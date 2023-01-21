dccc4f93d90fad2990a18484553ed225cb976a35
Roshni gupta <roshni05011995@gmail.com>
2023-01-21 23:03:39 +0530

    swagger

--- PaymentGuru/src/main/java/com/paymentGuru/PaymentGuruApplication.java
+++ PaymentGuru/src/main/java/com/paymentGuru/PaymentGuruApplication.java
@@ -1,10 +1,15 @@
 package com.paymentGuru;
 
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class PaymentGuruApplication {
 
 	public static void main(String[] args) {
